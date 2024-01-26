f40f3c2bf05cc6a6693b09b27f03ddfc5f207fd3
rjop-hccgt <92553742+rjop-hccgt@users.noreply.github.com>
2024-01-26 17:01:57 -0600

    fix(lint): Lint
    fixing linting issues
    
    Signed-off-by: rjop-hccgt <92553742+rjop-hccgt@users.noreply.github.com>

--- config/provider.go
+++ config/provider.go
@@ -7,6 +7,7 @@ package config
 import (
 	// Note(turkenh): we are importing this to embed provider schema document
 	_ "embed"
+
 	"github.com/healthcarecom/provider-okta/config/appbasicauth"
 	"github.com/healthcarecom/provider-okta/config/appbookmark"
 	"github.com/healthcarecom/provider-okta/config/appoauth"
